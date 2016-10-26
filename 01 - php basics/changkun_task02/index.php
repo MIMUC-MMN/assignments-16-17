8b85bb97c92533dc0cbcbd2fa1f5d3e1a41d9681
Changkun Ou <ouchangkun@outlook.com>
2016-10-26 18:15:51 +0200

    add: assignment-01-task-02, the php implement part.

--- 01 - php basics/changkun_task02/index.php
+++ 01 - php basics/changkun_task02/index.php
@@ -33,5 +33,57 @@ textarea {
     </form>
 </div>
 
+<?php
+
+//
+// shuffle a string
+// $str: string reference to be shuffled
+//
+function shuffle_str($str) {
+    // use regexp find all matche words, not include punctuation
+    preg_match_all('/([a-zA-Z]+)/', $str, $matches);
+
+    $new_str = $str;
+    $find_len = 0;
+    // shuffle all matches
+    foreach ($matches[1] as $word) {
+        $random = shuffle_word($word);
+
+        // find $word position in $new_str from $find_len
+        $pos = stripos($new_str, $word, $find_len);
+
+        // replace from $pos to $ random
+        $new_str = substr_replace($new_str, $random, $pos, strlen($random));
+
+        // only search the remaining part
+        $find_len += strlen($word)+1;
+    }
+    return $new_str;
+}
+
+//
+// shuffle a word
+// $word: word to be shuffled
+//
+function shuffle_word($word) {
+    // shuffle the middle part of a word
+    $middle = substr($word, 1, strlen($word)-2);
+    $middle = str_shuffle($middle);
+
+    // joint a new word
+    $random = substr_replace($word, $middle, 1, strlen($word)-2);
+    return $random;
+}
+
+if(isset($_POST['text'])) {
+    echo '<div id="app2" class="form-group">
+            <label>Shuffled results: </label>';
+    $result = shuffle_str($_POST['text']);
+    echo '<p>' . $result . '</p>';
+    echo '</div>';
+}
+
+?>
+
 </body>
 </html>

d51c7da6103be4ff762b8bc5f300a9aedb4dd11c
Changkun Ou <ouchangkun@outlook.com>
2016-10-26 18:14:42 +0200

    add: assignment-01-task-02, implement basic html structure.

---
+++ 01 - php basics/changkun_task02/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Word Sfuhle!</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<style type="text/css">
+#app {
+    padding: 20px;
+    margin: 20px;
+}
+textarea {
+    height: 500px;
+}
+#app2 {
+    padding: 20px;
+    margin: 20px;
+}
+</style>
+
+<body>
+
+<div id="app">
+    <form method="post">
+        <div class="form-group">
+            <label>Please put your text here...</label>
+            <textarea class="form-control" rows="8" name="text"></textarea>
+        </div>
+        <input class="btn btn-primary btn-block center-block"
+                type="submit" name="submit" value="Sffuhle it!">
+    </form>
+</div>
+
+</body>
+</html>

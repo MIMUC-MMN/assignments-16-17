cbb25721b0ba71be6e4937545cf83277f21400e5
zhenhaoli <zhenhao.li@outlook.com>
2016-10-28 23:59:39 +0200

    added utf-8 support for scrambler and prettified it a lil

--- 01 - php basics/zhenhaoli_task02/task2.php
+++ 01 - php basics/zhenhaoli_task02/task2.php
@@ -1,36 +1,33 @@
 <?php
 if (isset($_POST['textToScramble'])) {
-    $chars = preg_split('//u', $_POST['textToScramble'], -1, PREG_SPLIT_NO_EMPTY);
-    $words = [];
-    $makeWords = function ($c1, $c2) use (&$words) {
-        if ((ctype_alnum($c1) && ctype_alnum($c2))) return $c1 . $c2;
-        $words[] = $c1;
-        return $c2;
-    };
-    $words[] = array_reduce($chars, $makeWords);
     $scramble_word = function ($word) {
-        return strlen($word) <= 3 ? $word : $word[0] . str_shuffle(substr($word, 1, strlen($word) - 2)) . $word[strlen($word) - 1];
+        if(strlen($word)<=3) return $word;
+        $chars = preg_split('//u', $word, -1, PREG_SPLIT_NO_EMPTY); //avoiding multibyte handling by using array...
+        $chars_to_scramble = array_slice($chars, 1, count($chars)-2);
+        shuffle($chars_to_scramble);
+        return reset($chars).join("",$chars_to_scramble).end($chars);
     };
     $input = $_POST['textToScramble'];
-    $output = join("", array_map($scramble_word, $words));
+    preg_match_all('/\p{L}+|./u', $input, $m); //divide text by words and all other stuffs (supports utf-8)
+    $output = join("", array_map($scramble_word, $m[0]));
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MMN - Assignment 1 - Task 2 - Scrambler</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.7/journal/bootstrap.min.css">
 </head>
-<body>
-<div style="width: 50%; float: left">
-    <form method="post">
-        <textarea rows="30" cols="33" name="textToScramble"><?= isset($input) ? $input : "" ?></textarea>
-        <p><button type="submit">Scramble!</button></p>
-    </form>
-</div>
-<div style="width: 50%; float: right">
-    <h1>Output: </h1>
-    <p><?= isset($output) ? $output : "" ?></p>
+<body class="container-fluid">
+<form method="post" class="col-xs-12 col-sm-6" >
+    <h3>Input text below: </h3>
+    <textarea rows=10 class="form-control" name="textToScramble"><?= isset($input) ? $input : "" ?></textarea>
+    <button type="submit" class="btn btn-info btn-block" style="margin-top: 10px">Scramble!</button>
+</form>
+<div class="col-xs-12 col-sm-6">
+    <h3>Output: </h3><p><?= isset($output) ? $output : "" ?></p>
 </div>
 </body>
 </html>

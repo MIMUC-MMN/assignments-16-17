d4ffaac415426fe6f243fe1cdee5e7b6baebc590
Tobias Seitz <tobi@tobitobi.de>
2016-12-01 12:20:30 +0100

    skeleton for assignment 06

--- 06 - vanilla js/README.md
+++ 06 - vanilla js/README.md
@@ -2,3 +2,17 @@
 It's probably best if you somehow prefix your folder names with your initials / name / alias
 
 Like so: `darkwingDuck_task01`, `darkwingDuck_task02`... you get the idea 
+
+
+## Task 02 - RSS Ticker ##
+
+You don't have to adjust `xml-proxy.php`. Just put in into your Apache root directory.
+
+`C:\xampp\htdocs\ `  (Windows)
+
+`/Applications/XAMPP/htdocs/`  (macOS)
+
+
+Then go to http://localhost/xml-proxy.php 
+
+
---
+++ 06 - vanilla js/skeleton/xml-proxy.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tobi
+ * Date: 30/11/2016
+ * Time: 16:46
+ */
+
+
+header('Content-type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Method: *");
+
+
+error_reporting(E_ERROR);
+
+
+if (isset($_GET['url']) && !empty($_GET['url'])) {
+    $url = $_GET['url'];
+} else {
+    $url = 'http://feeds.feedburner.com/Techcrunch/europe';
+}
+
+$failureObj = array(
+    'status' => 'error',
+    'code' => 500,
+    'message' => 'Invalid url. The passed URL ' . $url . ' could not be loaded. ',
+    'reasons' => array('The page is not formatted with valid XML', 'The page is not available', 'The URL is invalid')
+);
+
+try {
+    $xml = simplexml_load_file($url);
+    $jsonFromXML = json_encode($xml);
+
+    // if json_encode fails, it - for some reason - returns 'false' as a String.
+    if ($jsonFromXML != 'false') {
+        echo $jsonFromXML;
+    } else {
+        echo json_encode($failureObj);
+    }
+
+} catch (Exception $e) {
+    json_encode($failureObj);
+    exit();
+}
+

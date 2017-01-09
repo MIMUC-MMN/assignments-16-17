24c002e6fef3a5bc5a745436f3d6d18585883131
Changkun Ou <hi@changkun.us>
2017-01-09 11:11:10 +0100

    (add): assignment 09 task 02 solution

--- 09 - nodejs misc/changkun_task02/app.js
+++ 09 - nodejs misc/changkun_task02/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var watermarker = require('./routes/watermarker');
+var stegano = require('./routes/steganographer');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use(watermarker);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
+app.use('/stegano', stegano);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
